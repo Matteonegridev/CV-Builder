74ca8d4d7c631aea30c8018f197412496d6fbced
Matteo <matteonegridev@gmail.com>
2024-10-30 15:47:40 +0100

    styled forms

--- src/Utils/Form.tsx
+++ src/Utils/Form.tsx
@@ -8,13 +8,13 @@ export function Form({ children }: FormProps) {
   const [showForm, setShowForm] = useState(false);
   return (
     <div
-      className={`flex flex-col gap-6 overflow-hidden p-6 shadow-md transition-all duration-500 ease-in-out ${
+      className={`mb-5 flex flex-col gap-6 overflow-hidden rounded-xl bg-slate-100 p-6 shadow-md shadow-stone-300 transition-all duration-500 ease-in-out ${
         showForm ? 'max-h-20' : 'max-h-full'
       }`}
     >
       <button
         onClick={() => setShowForm(!showForm)}
-        className="self-center justify-self-center rounded-md bg-blue-500 px-5 py-2 text-white ~text-base/xl"
+        className="self-center justify-self-center rounded-lg bg-blue-500 px-5 py-2 font-semibold text-white shadow-sm shadow-black ~text-base/xl"
       >
         {showForm ? 'Edit' : 'Save'} Info
       </button>
